a8b22958d560777f373d37935f1ca1a2881c94e7
IsaacChan96 <chanxiisaac@gmail.com>
2020-11-21 09:08:45 +0800

    first commit and push

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask,render_template,url_for,request
+import pandas as pd 
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.externals import joblib
+import pickle
+import nltk
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer 
+from profanity_check import predict as pf
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+	nltk.download('stopwords')
+	NB_spam_model = open('NB_spam_model.pkl','rb')
+	clf = joblib.load(NB_spam_model)
+
+	if request.method == 'POST':
+                pickle_in = open("cv.pickle","rb")
+                cv = pickle.load(pickle_in)
+                text = request.form['message']
+                review = text.split()
+                ps = PorterStemmer()
+                review= [ps.stem(word) for word in review if not word in set(stopwords.words("english"))]
+                message=" ".join(review)
+                data = [message]
+                vect = cv.transform(data).toarray()
+                my_prediction = str(round(clf.predict_proba(vect)[0][1]*100,1))+'%'
+                if pf(data)[0] == 1:
+                        my_prediction="0%"
+                
+	return render_template('result.html',prediction = my_prediction)
+
+
+app.run(debug=True)
